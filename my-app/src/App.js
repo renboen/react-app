380848a45e5c2afcf167d6e663040c3e97449bd4
任博恩 <308528611@qq.com>
2017-11-27 11:55:07 +0800

    基于create-react-app   react改端口   把webpack配置调出来

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,13 @@
 import React, { Component } from 'react';
-import logo from './componentstyle/imgs/logo.svg';
-
 import './componentstyle/css/App.css';
-import { Navbar, Jumbotron, Button } from 'react-bootstrap';
-// alert('%process.env.PUBLIC_URL%')
-// import logo from {%process.env.PUBLIC_URL%+'/favicon.ico'};
-// const logo=require( 'http://localhost:3000/favicon.ico')
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-   
+          app总入口
         </header>
    
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button>Left</Button>
-    <Button>Middle</Button>
-    <Button>Right</Button>
       </div>
     );
   }
