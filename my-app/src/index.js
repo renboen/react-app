d709817d2ce153162ccf262e3502bd7bced6bc4e
任博恩 <308528611@qq.com>
2017-11-27 10:56:25 +0800

    react-init多语言

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import zh from 'react-intl/locale-data/zh';
+import en from 'react-intl/locale-data/en'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import './componentstyle/css/index.css';
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import {addLocaleData,IntlProvider } from 'react-intl';
+import zh_CN from './lang/cn';
+import en_US from './lang/en';
+addLocaleData([...en,...zh]);
+ReactDOM.render(
+<IntlProvider
+locale={"zh"}
+messages={zh_CN}
+>
+    <App />
+</IntlProvider >
+, document.getElementById('root'));
 registerServiceWorker();
